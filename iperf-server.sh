b6cabd191bdc5cc1f3b6287a9284a38b87454c03
tkaufman914 <tkaufman@gmail.com>
2020-03-27 14:07:21 -0400

    Update iperf-server.sh

--- iperf-server.sh
+++ iperf-server.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
+echo
+echo
+echo "Usage: iperf-server.sh PORT INSTANCE"
+echo
+i=0
 
 PORT=$1
+INSTANCE=$2
 
 if [ -z $PORT ]
 then echo "Missing Starting Port Number" && exit 1
 fi
 
-SPORT=$(( $PORT / 10 ))
+if [ -z $INSTANCE ]
+then echo "Missing Instance Count" && exit 1
+fi
+echo
+echo
+echo
+echo "Starting Iperf..."
+echo
+echo
+echo
+sleep 2
+while [ $i -le $INSTANCE ]
 
-for i in {0..4}
-do iperf3 -s -p $SPORT$i &
+do
+	iperf3 -s -p $PORT &
+	i=$(( $i + 1  ))
+	PORT=$(( $PORT + 1))
+
 done
