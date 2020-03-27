a8e42d0bfed39ece2875dea285c5d9efab6a614f
tkaufman914 <tkaufman@gmail.com>
2020-03-27 11:06:11 -0400

    Update iperf-client.sh

--- iperf-client.sh
+++ iperf-client.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 TEST_NO=$2
 IP_ADDR=$1
-PORT=$(( $3 / 10 ))
+PORT=$3
+INSTANCE=$4
 i=0
 if [ -z $IP_ADDR ]
 then echo "Missing IP Address" && exit 1
@@ -13,8 +14,11 @@ if [ -z $PORT ]
 then echo "Missing Starting Port Number" && 1
 fi
 echo "Starting iperf3"
-while [ $i -le 5 ]
+while [ $i -le $INSTANCE ]
 do
-  iperf3 -c $IP_ADDR -t 10 -P5 -p $PORT$i --json >> ${IP_ADDR}-test${TEST_NO}-stream${i}.json &
+  echo $i
+  echo $PORT
+  echo "iperf3 -c $IP_ADDR -t 10 -P5 -p $PORT --json >> ${IP_ADDR}-test${TEST_NO}-stream${i}.json &"
   i=$(( $i + 1  ))
+  PORT=$(( $PORT + 1))
 done
